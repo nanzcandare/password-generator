35b613e5bc9b0ad02de9d6e5bf9e76cf2b4bf6cb
Nancy Candare <nancy.candare@gmail.com>
2024-06-21 23:57:42 +0800

    commit initial files

---
+++ index.js
@@ -0,0 +1,29 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passLengthEl = document.getElementById("pass-length")
+let passLength = passLengthEl.value
+let password1El = document.getElementById("password1")
+let password2El = document.getElementById("password2")
+
+function generateChar() {
+    let char = Math.floor(Math.random() * characters.length)
+    return characters[char]
+}
+
+function generatePassword() {
+    let pass = ""
+    passLength = passLengthEl.value
+    
+    for (let i = 0 ; i < passLength ; i++) {
+        pass += generateChar()
+    }
+    password1El.innerText = pass
+    pass = ""
+    
+    for (let i = 0 ; i < passLength ; i++) {
+        pass += generateChar()
+    }
+    password2El.innerText = pass
+    pass = ""
+}
